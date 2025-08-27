f16b1969e47aa5d486827421956003e3010892af
Rene <Rene.Mlodoch@gmail.com>
2025-08-27 15:58:06 +0200

    Update login.py

--- code/src/backend/routes/login.py
+++ code/src/backend/routes/login.py
@@ -6,7 +6,7 @@ import qrcode
 import io
 
 
-login_bp = Blueprint('login', __name__, url_prefix='/login')
+login_bp = Blueprint('login', __name__, url_prefix='/login', template_folder='../../frontend/templates')
 
 @login_bp.route('/', methods=['GET', 'POST'])
 def login():
