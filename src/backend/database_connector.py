6c6e66d9d767b2e43ba74aaa2bbb14410eb7ed2e
freshgamingYT <rene.mlodoch@gmail.com>
2025-08-28 14:02:30 +0200

    test

--- src/backend/database_connector.py
+++ src/backend/database_connector.py
@@ -1,12 +1,23 @@
 import mariadb
+import time
 
-conn = mariadb.connect(
-    username='barbot',
-    password='Keins123!',
-    host='db',
-    port=3306,
-    database='BarbotDB'
-)
+for attempt in range(10):
+    try:
+        conn = mariadb.connect(
+            username='barbot',
+            password='Keins123!',
+            host='db',
+            port=3306,
+            database='BarbotDB'
+        )
+        print('Successfully connected to database')
+        break
+    except mariadb.Error as e:
+        print(f'Versuch {attempt + 1}: Verbindung zur Datenbank fehlgeschlagen. Felder Sie {e}')
+        time.sleep(2)
+else:
+    raise Exception('Could not connect to database')
+
 
 cur = conn.cursor()
 
